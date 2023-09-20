3647b796fc1f2a21dcb04c990252eee5134d67d9
Steven Walton <walton.stevenj@gmail.com>
2023-09-20 10:33:15 -0700

    Moved things to own folders and edited install

--- shellfirm-plugin.sh
+++
@@ -1,8 +0,0 @@
-shellfirm-pre-command () {
-    if [[ "${BUFFER}" == *"shellfirm pre-command"* ]]; then
-        return
-    fi
-    shellfirm pre-command --command "${BUFFER}"
-    zle .accept-line
-}
-zle -N accept-line shellfirm-pre-command
