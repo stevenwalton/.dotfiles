9bc5c675529657ac94435aa962072172f6a300b1
Steven Walton <walton.stevenj@gmail.com>
2024-07-09 22:24:04 -0700

    Cleanup aliases and ssh-agent
    Cleaning up some of the aliases and just formatting a little better.
    
    I think this better handles the ssh-agent issue. Pushing to test on nix
    machines.

--- rc_files/zsh/basic_aliases.zsh
+++ rc_files/zsh/basic_aliases.zsh
@@ -1,25 +1,26 @@
-####################
-# Normal Aliases
-# This should be second because we might replace some commands
-####################
+################################################################################
+# Defined aliases for basic and standard commands
+# This should be second because we might replace some of the core utils. Thus
+# this file is separated to make that easier.
+# So restrict this file to things more like core-utils 
+################################################################################
 
 alias rm='rm -I' # prompt when >3 files being deleted
 alias vi='vim'
 alias ssh='ssh -YC'
+
 # Tree replacement if don't have tree
 if ! (_exists tree)
 then
     alias tree='find . | sed -e "s/[^-][^\/]*\// |/g" -e "s/|\([^ ]\)/|-\1/"'
 fi
 # Grep to have color, give line number, don't tell me it can't access restricted files (sudo), and don't process binary files (garbage output ):
+# NOTE!!!!: This can on occasion cause the actual text of the output to change
+# and fuck up piped commands. If that happens use `\grep` or `grep
+# --color=never`.
+# We might run into this bullshit: https://x.com/WaltonStevenj/status/1809378443655840238
 alias grep='grep --color=always --no-messages --binary-files=without-match'
 
-# Tmux doesn't like to recognize 256 colouring, so let's force it
-# -u fixes backspace error thing
-if (_exists tmux)
-then
-    alias tmux='tmux -2 -u'
-fi
 
 alias df='df -h'
 alias log='git log --graph --oneline --decorate'
