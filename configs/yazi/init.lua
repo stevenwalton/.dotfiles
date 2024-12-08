b94ea1a273e56f3f6175a6a1d3bd4019eb3dfb39
Steven Walton <walton.stevenj@gmail.com>
2024-12-07 16:33:08 -0800

    Yazi previewer fixed

--- configs/yazi/init.lua
+++ configs/yazi/init.lua
@@ -15,7 +15,7 @@ function Linemode:size_and_mtime()
 	end
 
 	local size = self._file:size()
-	return ui.Line(string.format("%s | %s", size and ya.readable_size(size) or "-", time))
+	return ui.Line(string.format("%s %s", size and ya.readable_size(size) or "-", time))
 end
 
 -- require("mime-ext"):setup {
